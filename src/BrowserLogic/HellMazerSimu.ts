ceaebf750af16544acd3fd9451a538ef6b60d71d
Danny Student <dannygeisz@berkeley.edu>
2020-10-26 15:58:24 -0600

    Basically just shuffled stuff

--- src/BrowserLogic/HellMazerSimu.ts
+++ src/BrowserLogic/HellMazerSimu.ts
@@ -1,3 +1,5 @@
+
+
 export class HellMazerSimu {
     private frame_rate: number;
     /** Time width of the maze*/
@@ -8,18 +10,11 @@ export class HellMazerSimu {
     /** Side length of the tile in pixels*/
     private tile_side_length: number;
 
-    /** Mazer x and y position*/
-    private x: number;
-    private y: number;
-
     constructor(maze_width: number, maze_height: number, tile_side_length, frame_rate: number) {
         this.maze_height = maze_height;
         this.maze_width = maze_width;
         this.frame_rate = frame_rate;
         this.tile_side_length = tile_side_length;
-
-        this.x = 0;
-        this.y = 0;
     }
 
     run() {
@@ -32,6 +27,4 @@ export class HellMazerSimu {
         syncWithEywa();
         updateHellMazer();
     }
-
-
 }
